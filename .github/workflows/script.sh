517110980aa98ddaf8a7f914f7bb93855c6effe7
Brett Terpstra <me@brettterpstra.com>
2022-10-11 12:56:23 -0500

    Updating repolinter pipeline to require Oracle disclaimer

---
+++ .github/workflows/script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) 2021 Oracle and/or its affiliates.
+#
+
+NUM_COMMITS=`curl -s \
+  -H "Accept: application/vnd.github.v3+json" \
+  ${INPUT_PULL_URL} \
+  | jq .commits`
+FETCH_DEPTH=`echo $NUM_COMMITS+1 | bc`
+
+echo "There are ${NUM_COMMITS} commits."
+echo "Fetch depth should be ${FETCH_DEPTH}"
+
+echo "::set-output name=num_commits::${NUM_COMMITS}"
+echo "::set-output name=fetch_depth::${FETCH_DEPTH}"
